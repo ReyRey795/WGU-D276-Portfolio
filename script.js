fea57941e2cdec77ee8fa6e5f14d4729b708235a
Reynaldo Serrano <serranoj.rreynaldo@gmail.com>
2026-08-23 18:23:22 -0500

    Removed js file since rubric wanted JS inline.

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const dateContainer = document.getElementById("current-date");
-const currentDate = new Date();
-
-dateContainer.textContent = `Current Date: ${currentDate.toLocaleDateString()}`;
-
-console.log("Resume page loaded successfully.");
