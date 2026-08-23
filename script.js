231698d15c93f34a196890fe4d526dfbfb818e3f
ReyRey795 <serranoj.rreynaldo@gmail.com>
2026-08-23 12:19:24 -0500

    Display cukrrent date on resume

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const dateContainer = document.getElementById("current-date");
+const currentDate = new Date();
+
+dateContainer.textContent = `Current Date: ${currentDate.toLocaleDateString()}`;
