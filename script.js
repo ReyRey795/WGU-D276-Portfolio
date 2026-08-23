76ab649cc43f57806887c93cc4a94fc76e9ed6ca
ReyRey795 <serranoj.rreynaldo@gmail.com>
2026-08-23 12:20:54 -0500

    Add resume console output to state page loaded successfully.

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const dateContainer = document.getElementById("current-date");
 const currentDate = new Date();
 
 dateContainer.textContent = `Current Date: ${currentDate.toLocaleDateString()}`;
+
+console.log("Resume page loaded successfully.");
